bb57ecfcd556d690d2bdd12a5a62003ac3c17e8a
Jeffrey Bride <jbride@zareason.com>
2013-10-17 14:24:45 -0600

    completed annotation_visibility POC

--- jee/modules/annotation_visibility/bean/src/main/java/com/redhat/gpe/bean/Bean.java
+++ jee/modules/annotation_visibility/bean/src/main/java/com/redhat/gpe/bean/Bean.java
@@ -9,23 +9,27 @@ import javax.ejb.Singleton;
 import javax.ejb.Startup;
 import javax.persistence.Id;
 
+import com.redhat.gpe.domain.Customer;
+
 @Singleton
 @Startup
 public class Bean {
 
     @PostConstruct
     public void init() throws ClassNotFoundException{
-        Class varClass = Class.forName("com.redhat.gpe.domain.Customer");
+        Customer cObj = new Customer();
+        //Class varClass = Class.forName("com.redhat.gpe.domain.Customer");
+        Class varClass = cObj.getClass();
         System.out.println("init() class = "+varClass);
         do {
-                Field[] fields = varClass.getDeclaredFields();
-                for (int i = 0; i < fields.length; i++) {
-                    Field field = fields[i];
-                    Id id = field.getAnnotation(Id.class);
-                    if (id != null) {
-                       System.out.println("******* FOUND ANNOTATED FIELD **************");
-                    }
+            Field[] fields = varClass.getDeclaredFields();
+            for (int i = 0; i < fields.length; i++) {
+                Field field = fields[i];
+                Id id = field.getAnnotation(Id.class);
+                if (id != null) {
+                   System.out.println("******* FOUND ANNOTATED FIELD **************");
                 }
+            }
         } while ((varClass = varClass.getSuperclass()) != null);
     }
 }
