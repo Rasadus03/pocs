214879ff383ff1bd11a4536a1947987e1f7e5da6
jbride <jeff bride>
2015-06-04 23:09:47 -0600

    latest

--- vertx/HttpServer.java
+++ vertx/HttpServer.java
@@ -1,44 +1,10 @@
-import io.vertx.core.Vertx;
-import io.vertx.core.VertxOptions;
 import io.vertx.core.AbstractVerticle;
-import java.util.function.Consumer;
 
 // execute: /opt/jboss/vert.x-3.0.0-milestone6/bin/vertx run HttpServer.java
 /*
  * * @author <a href="http://tfox.org">Tim Fox</a>
  * */
 public class HttpServer extends AbstractVerticle {
-
-    private static final String CORE_EXAMPLES_DIR = "core-examples";
-    private static final String CORE_EXAMPLES_JAVA_DIR = CORE_EXAMPLES_DIR + "/src/main/java/";
-
-    public static void main(String[] args) {
-        Class verticleId = HttpServer.class;
-        String exampleDir = CORE_EXAMPLES_JAVA_DIR + verticleId.getPackage().getName().replace(".", "/");
-        VertxOptions options = new VertxOptions().setClustered(false);
-        System.setProperty("vertx.cwd", exampleDir);
-
-        Consumer<Vertx> runner = vertx -> {
-            try {
-                vertx.deployVerticle(verticleId.getName());
-            } catch (Throwable t) {
-                t.printStackTrace();
-            }
-        };
-        if (options.isClustered()) {
-            Vertx.clusteredVertx(options, res -> {
-                if (res.succeeded()) {
-                    Vertx vertx = res.result();
-                    runner.accept(vertx);
-                } else {
-                    res.cause().printStackTrace();
-                }
-            });
-        } else {
-            Vertx vertx = Vertx.vertx(options);
-            runner.accept(vertx);
-        }
-    }
 
     public void start() throws Exception {
         vertx.createHttpServer().requestHandler(req -> {
